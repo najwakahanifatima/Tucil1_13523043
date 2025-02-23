28262e9962532f21766d49a1ae45636b184ead2d
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2025-02-23 22:09:45 +0700

    fix: game null handling

--- src/Main.java
+++ src/Main.java
@@ -17,31 +17,33 @@ public class Main {
 
             Game game = Setup.gameSetup(filename);
 
-            Solver.board = new boolean[game.boardRows()][game.boardCols()];
+            if (game != null){
+                Solver.board = new boolean[game.boardRows()][game.boardCols()];
 
-            Instant start = Instant.now();
+                Instant start = Instant.now();
 
-            if (Solver.fitBlocksToBoard(game.gameBlocks(), new ArrayList<>(), Solver.board)){
-                System.out.println("Berhasil.");
-                Solver.showSolution(Solver.solution);
-            } else {
-                System.err.println("Gagal.");
-            }
+                if (Solver.fitBlocksToBoard(game.gameBlocks(), new ArrayList<>(), Solver.board)){
+                    System.out.println("\nSucceed.\n");
+                    Solver.showSolution(game, Solver.solution);
+                } else {
+                    System.err.println("\nSolution not found.");
+                }
 
-            Instant end = Instant.now();
-            Duration duration = Duration.between(start, end);
+                Instant end = Instant.now();
+                Duration duration = Duration.between(start, end);
 
-            // Print durasi pencarian
-            System.out.println("Cases reviewed: " + Solver.counter + " cases.");
-            System.out.println("Duration: " + duration.toMillis() + "ms.");
+                // Print durasi pencarian
+                System.out.println("\nCases reviewed: " + Solver.counter + " cases.");
+                System.out.println("Duration: " + duration.toMillis() + "ms.");
 
-            // Save solution option
-            System.out.print("\nDo you want to save the solution? (yes/no): ");
-            String saveChoice = scanner.nextLine();
-            if (saveChoice.equals("yes")){
-                System.out.print("Input file name (without .txt): ");
-                String fileOutput = scanner.nextLine();
-                Setup.saveSolution(fileOutput, duration);
+                // Save solution option
+                System.out.print("\nDo you want to save the solution? (yes/no): ");
+                String saveChoice = scanner.nextLine();
+                if (saveChoice.equals("yes")){
+                    System.out.print("Input file name (without .txt): ");
+                    String fileOutput = scanner.nextLine();
+                    Setup.saveSolution(fileOutput, duration);
+                }
             }
         }
 
