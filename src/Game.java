3af9ab1b4400d4655eb7501ed395ed5572f686df
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2025-02-23 16:51:25 +0700

    feat: update methods and attributes in Game

--- src/Game.java
+++ src/Game.java
@@ -1,33 +1,37 @@
 import java.util.ArrayList;
 
 public class Game {
-    private static int rows;
-    private static int cols;
-    private static int numBlocks;
-    private static String conf;
-    private static ArrayList<Block> gameBlocks;
+    private int rows;
+    private int cols;
+    private int numBlocks;
+    private String conf;
+    private ArrayList<Block> gameBlocks;
 
     public Game(int N, int M, int P, String S, ArrayList<Block> blocks){
-        Game.rows = N;
-        Game.cols = M;
-        Game.numBlocks = P;
-        Game.conf = S;
-        Game.gameBlocks = blocks;
+        rows = N;
+        cols = M;
+        numBlocks = P;
+        conf = S;
+        gameBlocks = blocks;
     }
 
-    // public static int boardRows(){
-    //     return Game.rows;
-    // }
+    public int boardRows(){
+        return rows;
+    }
 
-    // public static int boardCols(){
-    //     return Game.cols;
-    // }
+    public int boardCols(){
+        return cols;
+    }
 
-    // public static int numBlocks(){
-    //     return Game.numBlocks;
-    // }
+    public int numBlocks(){
+        return numBlocks;
+    }
 
-    // public static String conf(){
-    //     return Game.conf;
-    // }
+    public String conf(){
+        return conf;
+    }
+
+    public ArrayList<Block> gameBlocks(){
+        return gameBlocks;
+    }
 }
